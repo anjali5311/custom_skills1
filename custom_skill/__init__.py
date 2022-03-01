47dd1415c37b629f78b7470cb2ed81a6bd822c99
anjali5311 <anjali.arora@celebaltech.com>
2022-03-01 19:21:51 +0530

    second_commit

--- custom_skill/__init__.py
+++ custom_skill/__init__.py
@@ -1,18 +1,35 @@
 import logging
 
 import azure.functions as func
-import spacy
-nlp = spacy.load('en_core_web_sm')
+from nltk import word_tokenize, pos_tag, ne_chunk
+import nltk
+import json
+from nltk import RegexpParser
+from nltk import Tree
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
 
-def get_noun_phrases(text):
+def get_noun_phrases(text, chunk_func=ne_chunk):
+    continuous_chunk=[]
     if isinstance(text,str):
-        noun_phrases=[]
-        doc = nlp(text)
-        for nounpphrase in doc.noun_chunks:
-            noun_phrases.append(nounpphrase.text)
-        return noun_phrases
-    else:
-        return []
+        chunked = chunk_func(pos_tag(word_tokenize(text)))
+        continuous_chunk = []
+        current_chunk = []
+
+        for subtree in chunked:
+            if type(subtree) == Tree:
+                current_chunk.append(" ".join([token for token, pos in subtree.leaves()]))
+            elif current_chunk:
+                named_entity = " ".join(current_chunk)
+                if named_entity not in continuous_chunk:
+                    continuous_chunk.append(named_entity)
+                    current_chunk = []
+            else:
+                continue
+
+    return continuous_chunk
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # {'content':'this is sample text'}
@@ -24,10 +41,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         except ValueError:
             pass
         else:
-            content = req_body.get('content')
+            content = req_body['content']
 
     if content:
-        noun_phrases=get_noun_phrases(text)
-        return func.HttpResponse(noun_phrases,status_code=200)
+        noun_phrases=get_noun_phrases(content)
+        return func.HttpResponse(json.dumps({'noun_phrases':noun_phrases}),status_code=200)
     else:
-        return func.HttpResponse([], status_code=404)
+        return func.HttpResponse(json.dumps({'noun_phrases':[]}),status_code=404)
